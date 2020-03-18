fa48ba1e217f93deff4d861c955fe68b0a46bb3b
Bruno Nascimento <brunonascimento@RBSs-Macbook-Pro-03.local>
2020-03-17 22:23:46 -0300

    Added Procfile

--- src/database/database.js
+++ src/database/database.js
@@ -1,14 +1,13 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  'mongodb+srv://brunonascimento:dAwtGfZjrJN6Y4Ta@cluster0-yv9b6.gcp.mongodb.net/ICM?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
