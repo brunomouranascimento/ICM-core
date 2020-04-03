45c266324001cf5879c5da41a60c04ae6b4fc555
Bruno Nascimento <bruno.bmn@gmail.com>
2020-04-02 23:08:45 -0300

    Added compreesion to responses

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const compression = require('compression');
 const cors = require('cors');
 
 const app = express();
 
+app.use(compression());
 app.use(express.json());
 app.use(cors());
 
